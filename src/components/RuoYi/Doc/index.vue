f750d71c933c1add98a771aecc91f0e280953f44
陈永丰 <yongfeng.chen@genlot.com>
2024-10-08 13:57:17 +0800

    Initial commit with my changes

---
+++ src/components/RuoYi/Doc/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <svg-icon icon-class="question" @click="goto" />
+  </div>
+</template>
+
+<script setup>
+const url = ref('http://doc.ai-interview.vip/ai-interview-vue');
+
+function goto() {
+  window.open(url.value)
+}
+</script>
